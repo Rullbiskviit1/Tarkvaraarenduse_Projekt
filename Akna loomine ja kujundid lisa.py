c02d6396aab6a4d637a96f8f25f58931efafd8a8
Steven Jürimäe <steven.jurimae@vikk.ee>
2026-05-27 09:56:56 +0300

    PyGame

--- Akna loomine ja kujundid lisa.py
+++ Akna loomine ja kujundid lisa.py
@@ -1,90 +1,99 @@
-from tkinter import *
+import pygame
 
-# 1. Akna loomine ja seadistamine
-aken = Tk()
-aken.geometry("300x300")
-aken.title("Lumemees – Steven Jürimäe")
+# 1. Initsialiseerime PyGame'i ja tekitame akna
+pygame.init()
+aken = pygame.display.set_mode((300, 300))
+pygame.display.set_caption("Lumemees – Steven Jürimäe")
 
-# Tahvli loomine (taust helesinine)
-tahvel = Canvas(aken, width=300, height=300, bg="lightblue")
-tahvel.pack()
+# Värvid (RGB)
+HELESINIHE = (173, 216, 230)
+KOLLANE = (255, 255, 0)
+KULDNE = (255, 215, 0)
+VALGE = (255, 255, 255)
+MUST = (0, 0, 0)
+PRUUN = (139, 69, 19)
+KHAKI = (240, 230, 140)
+ORANZ = (255, 165, 0)
 
-# TAUSTA OBJEKTID (Joonistame kõigepealt)
+kell = pygame.time.Clock()
 
-# Päikeseketas
-tahvel.create_oval(15, 15, 65, 65, fill="yellow", outline="gold")
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-# Põhisuunad (üles, alla, vasakule, paremale)
-tahvel.create_line(40, 10, 40, 0, fill="gold", width=3)           # Kiir üles (UUS)
-tahvel.create_line(40, 70, 40, 85, fill="gold", width=3)          # Kiir alla
-tahvel.create_line(10, 40, 0, 40, fill="gold", width=3)           # Kiir vasakule (UUS)
-tahvel.create_line(70, 40, 85, 40, fill="gold", width=3)          # Kiir paremale
+    # Tausta täitmine
+    aken.fill(HELESINIHE)
 
-# Diagonaalsed suunad
-tahvel.create_line(20, 20, 5, 5, fill="gold", width=3)            # Kiir üles-vasakule (UUS)
-tahvel.create_line(60, 20, 75, 5, fill="gold", width=3)           # Kiir üles-paremale
-tahvel.create_line(20, 60, 5, 75, fill="gold", width=3)           # Kiir alla-vasakule
-tahvel.create_line(60, 60, 75, 75, fill="gold", width=3)          # Kiir alla-paremale
+    # --- TAUSTA OBJEKTID ---
+    # Päike
+    pygame.draw.ellipse(aken, KOLLANE, [15, 15, 50, 50])
+    pygame.draw.ellipse(aken, KULDNE, [15, 15, 50, 50], 2)
 
-# 5. Kolm pilve (koosnevad mitmest ülekattega ringist)
-# Pilv 1 (üleval paremal)
-tahvel.create_oval(190, 20, 230, 40, fill="white", outline="white")
-tahvel.create_oval(210, 10, 250, 35, fill="white", outline="white")
-tahvel.create_oval(230, 20, 270, 40, fill="white", outline="white")
+    # Päikesekiired
+    kiired = [
+        ((40, 10), (40, 0)),  # Üles
+        ((40, 70), (40, 85)),  # Alla
+        ((10, 40), (0, 40)),  # Vasakule
+        ((70, 40), (85, 40)),  # Paremale
+        ((20, 20), (5, 5)),  # Üles-vasak
+        ((60, 20), (75, 5)),  # Üles-parem
+        ((20, 60), (5, 75)),  # Alla-vasak
+        ((60, 60), (75, 75))  # Alla-parem
+    ]
+    for algus, lopp in kiired:
+        pygame.draw.line(aken, KULDNE, algus, lopp, 3)
 
-# Pilv 2 (keskel vasakul, päikese all)
-tahvel.create_oval(10, 110, 40, 130, fill="white", outline="white")
-tahvel.create_oval(25, 100, 60, 125, fill="white", outline="white")
-tahvel.create_oval(45, 110, 75, 130, fill="white", outline="white")
 
-# Pilv 3 (keskel paremal)
-tahvel.create_oval(220, 90, 250, 110, fill="white", outline="white")
-tahvel.create_oval(235, 80, 270, 105, fill="white", outline="white")
-tahvel.create_oval(255, 90, 285, 110, fill="white", outline="white")
+    # Pilved (koosnevad mitmest ellipsist)
+    def joonista_pilv(x, y):
+        pygame.draw.ellipse(aken, VALGE, [x, y + 10, 40, 20])
+        pygame.draw.ellipse(aken, VALGE, [x + 20, y, 40, 25])
+        pygame.draw.ellipse(aken, VALGE, [x + 40, y + 10, 40, 20])
 
-# LUMEMEHE TAGUMISED DETAILID
 
-# 1. Käed (pruunid jooned)
-# Vasak käsi
-tahvel.create_line(120, 150, 60, 130, width=4, fill="saddlebrown")
-# Parem käsi
-tahvel.create_line(180, 150, 240, 130, width=4, fill="saddlebrown")
+    joonista_pilv(190, 10)  # Pilv 1
+    joonista_pilv(10, 100)  # Pilv 2
+    joonista_pilv(220, 80)  # Pilv 3
 
-# 3. Kätte hari (paremasse kätte)
-# Luuavars
-tahvel.create_line(230, 230, 250, 70, width=3, fill="saddlebrown")
-# Luuaots (harjased) - kolmnurk
-tahvel.create_polygon(250, 70, 235, 30, 265, 30, fill="khaki", outline="saddlebrown")
+    # --- LUMEMEHE TAGUMISED DETAILID ---
+    # Käed
+    pygame.draw.line(aken, PRUUN, (120, 150), (60, 130), 4)
+    pygame.draw.line(aken, PRUUN, (180, 150), (240, 130), 4)
 
-# LUMEMEHE KEHA
+    # Hari/Luud
+    pygame.draw.line(aken, PRUUN, (230, 230), (250, 70), 3)
+    pygame.draw.polygon(aken, KHAKI, [(250, 70), (235, 30), (265, 30)])
+    pygame.draw.polygon(aken, PRUUN, [(250, 70), (235, 30), (265, 30)], 1)
 
-# Nihutatud veidi allapoole, et kübar ära mahuks
-# Alumine pall
-tahvel.create_oval(80, 190, 220, 290, fill="white", outline="black")
-# Keskmine pall
-tahvel.create_oval(100, 100, 200, 200, fill="white", outline="black")
-# Ülemine pall (pea)
-tahvel.create_oval(115, 40, 185, 110, fill="white", outline="black")
+    # --- LUMEMEHE KEHA ---
+    # Alumine pall
+    pygame.draw.ellipse(aken, VALGE, [80, 190, 140, 100])
+    pygame.draw.ellipse(aken, MUST, [80, 190, 140, 100], 1)
+    # Keskmine pall
+    pygame.draw.ellipse(aken, VALGE, [100, 100, 100, 100])
+    pygame.draw.ellipse(aken, MUST, [100, 100, 100, 100], 1)
+    # Pea
+    pygame.draw.ellipse(aken, VALGE, [115, 40, 70, 70])
+    pygame.draw.ellipse(aken, MUST, [115, 40, 70, 70], 1)
 
-# LUMEMEHE ESIMESED DETAILID
+    # --- KÜBAR ---
+    pygame.draw.rect(aken, MUST, [105, 35, 90, 10])  # Äär
+    pygame.draw.rect(aken, MUST, [125, 5, 50, 30])  # Toru
 
-# 2. Kübar (koosneb kahest ristkülikust)
-# Kübara äär
-tahvel.create_rectangle(105, 35, 195, 45, fill="black")
-# Kübara ülemine toru
-tahvel.create_rectangle(125, 5, 175, 35, fill="black")
+    # --- NÄGU JA NÖÖBID ---
+    # Silmad
+    pygame.draw.ellipse(aken, MUST, [135, 60, 10, 10])
+    pygame.draw.ellipse(aken, MUST, [155, 60, 10, 10])
+    # Nina
+    pygame.draw.polygon(aken, ORANZ, [(150, 75), (150, 85), (175, 80)])
+    pygame.draw.polygon(aken, MUST, [(150, 75), (150, 85), (175, 80)], 1)
+    # Nööbid
+    for y in [120, 145, 170]:
+        pygame.draw.ellipse(aken, MUST, [145, y, 10, 10])
 
-# Silmad
-tahvel.create_oval(135, 60, 145, 70, fill="black")
-tahvel.create_oval(155, 60, 165, 70, fill="black")
+    pygame.display.flip()
+    kell.tick(60)
 
-# Porgandnina
-tahvel.create_polygon(150, 75, 150, 85, 175, 80, fill="orange", outline="black")
-
-# Nööbid keskmisele pallile
-tahvel.create_oval(145, 120, 155, 130, fill="black")
-tahvel.create_oval(145, 145, 155, 155, fill="black")
-tahvel.create_oval(145, 170, 155, 180, fill="black")
-
-# Programmi töös hoidmine
-aken.mainloop()
+pygame.quit()
